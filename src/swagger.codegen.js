a9c0386999c96eefdf969c5b6f5846e8b694c6f8
Glen Kleidon <glenk@galkam.com.au>
2021-09-08 03:05:54 +1000

    Updated Codegen to track if lines.

--- src/swagger.codegen.js
+++ src/swagger.codegen.js
@@ -1,12 +1,13 @@
 class IfLine {
 
-   reset() {
-        this.lineNo = -1; 
-        this.count = 0; 
+    reset() {
+        this.lineNo = -1;
+        this.count = 0;
         this.content = "";
-        this.blankLineCount = 0;
-        this.outputAndReset = false;
-   }
+        this.LastAllowedBlankLine=0;
+        this.LastAllowedLine=0;
+        this.skip = false;
+    }
 
     constructor(text, lineNo) {
         this.reset();
@@ -14,45 +15,53 @@ class IfLine {
     }
 
     isIfLine(line, lineNo) {
-       
-        if (line && line.trim().startsWith('#IF'))
-        {
+
+        let result = (line && line.trim().startsWith('#IF'));
+        if (result) {
             this.lineNo = lineNo;
-            
+
             let firstSpace = line.indexOf(' ');
             this.content = line.substring(firstSpace).trim();
             let count = parseInt(
-                line.substring(3,firstSpace)
-                .replace('[','')
-                .replace(']','')
-                .trim());
-            
+                line.substring(3, firstSpace)
+                    .replace('[', '')
+                    .replace(']', '')
+                    .trim());
+
             this.count = (count) ? count : 1; // 1 blank line allowed by default
+            this.LastAllowedLine = lineNo+count;
+            this.LastAllowedBlankLine = this.LastAllowedLine-1;
+            this.skip = true;
         }
-        return this.lineNo>=0;
+        return result;
     }
 
-    getContent(line, lineNo){
-
-        let currentline = this.lineNo;
-        if (this.isIfLine(line,lineNo)) return "";
-
+    getContent(line, lineNo) {
+        this.skip = false;
+        let currentLineNo = this.lineNo;
+        let lineIsBlank = (line || '').trim() === '';
+        let alwaysReset = (lineNo > this.LastAllowedLine)
+        let resetIfBlank = ((lineNo > this.LastAllowedBlankLine));
         
-    }
+        let anotherIfLine = (!lineIsBlank && this.isIfLine(line, lineNo));
 
+        if ((anotherIfLine) || (lineIsBlank && !resetIfBlank)) return "";
+        let content = (alwaysReset) ? "" :this.content;
+        this.reset();
+        return content;
+    }
 }
 
 class CodeGen {
-    constructor()
-    {
+    constructor() {
 
     }
 
     setElementText(id, text) {
         let el = document.getElementById(id);
-        if (el) el.innerText =  text;
+        if (el) el.innerText = text;
     }
-    
+
 }
 
 const cg = new CodeGen();
